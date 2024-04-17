d1160a30931ae4c2d5481de47e4b0532a85935c2
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2024-04-17 16:32:44 -0300

    Add Docker and Podman installation options
    This commit introduces installation options for new or old Docker versions on Ubuntu or Debian systems and Podman. It contributes improvements to script 'dependencies.sh'. Now, it identifies the system version and proceeds with specific Docker installation, according to the user's choice. Podman installation includes staff such as Git, GCC, Make, and adjustments to Bash environment settings.

--- containers-aging/dependencies.sh
+++ containers-aging/dependencies.sh
@@ -3,23 +3,57 @@
 source /etc/os-release
 DISTRO_ID="$ID"
 DISTRO_CODENAME="$VERSION_CODENAME"
+SYSTEM_VERSION="$VERSION_ID"
 
-apt install python3.11-venv
+INSTALL_DOCKER_UBUNTU_OLD () {
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  VERSION_STRING="5:20.10.13~3-0~ubuntu-$DISTRO_CODENAME"
+  apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+
+}
+
+INSTALL_DOCKER_UBUNTU_NEW () {
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  VERSION_STRING="5:26.0.1-1~ubuntu.$SYSTEM_VERSION~$DISTRO_CODENAME"
+  apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+
+INSTALL_DOCKER_DEBIAN_OLD () {
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  bullseye stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  VERSION_STRING="5:20.10.13~3-0~debian-bullseye"
+  apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+INSTALL_DOCKER_DEBIAN_MEW () {
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  bullseye stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  VERSION_STRING="5:26.0.1-1~debian.11~bullseye"
+  apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+apt install python3.11 python3.11-venv || exit 1
 
 python3.11 -m venv env
 
+source env/bin/activate
+
 pip install PyYAML
-
-soruce env/bin/activate
-
-echo -e "Are you using a local server? If so, type the remote server ip address, else, press enter:"
-read -r ip
-if [ -z "$ip" ]; then
-  echo "No remote server ip address provided, skipping..."
-else
-  ssh-keygen
-  ssh-copy-id -i /root/.ssh/id_rsa.pub root@"$ip"
-fi
 
 echo -e "Would you like to install KVM? 1 - [Yes] 2 - [No]"
 read -r vm
@@ -47,17 +81,56 @@ fi
 echo -e "Which service are you using? 1 - [Docker] 2 - [Podman] 3 - None"
 read -r service
 if [ "$service" == "1" ]; then
-  apt install curl -y
-  echo "Installing Docker..."
-  curl -fsSL https://get.docker.com -o get-docker.sh
-  sh get-docker.sh
-  echo "Docker installed!"
-  rm -f get-docker.sh
+  echo -e "Are you using the latest version of Docker? 1 - [yes] 2 - [no]"
+  read -r service
+
+  apt-get install ca-certificates curl
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
+
+  if [ "$stap" == "1" ]; then
+    case $DISTRO_ID in
+    "ubuntu") INSTALL_DOCKER_UBUNTU_NEW;;
+    "debian") INSTALL_DOCKER_DEBIAN_MEW;;
+    esac
+  else
+    case $DISTRO_ID in
+    "ubuntu") INSTALL_DOCKER_UBUNTU_OLD;;
+    "debian") INSTALL_DOCKER_DEBIAN_OLD;;
+    esac
+  fi
+
+  groupadd docker
+  usermod -aG docker $USER
+  newgrp docker
+  docker run hello-world
+
 elif [ "$service" == "2" ]; then
-  pip install podman-compose
+  apt install git gcc make wget -y
+  apt-get install -y libsystemd-dev libgpgme-dev libseccomp-dev -y
+  echo "export PKG_CONFIG_PATH=/usr/lib/pkgconfig" >> ~/.bashrc
+  source ~/.bashrc
+
+  wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
+
+  echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+  source ~/.bashrc
+
   echo "Installing Podman..."
-  apt-get install podman -y
-  echo "Podman installed!"
+  git clone https://github.com/containers/podman.git
+  cd podman || exit 1
+  git checkout v4.9
+  make install.tools
+  make binaries
+
+  echo "export PATH=$PATH:~/podman/bin" >> ~/.bashrc
+  source ~/.bashrc
+
+  pip install podman-compose
+
+  podman run docker.io/hello-world
 else
   echo "Invalid option, exiting..."
   exit 1
