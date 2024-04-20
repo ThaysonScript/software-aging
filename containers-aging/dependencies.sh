2deb248ee05b339eda2fe09b6b5da3dd350f729a
ThaysonScript <thaysonguedes01@gmail.com>
2024-04-20 00:34:28 -0300

    resolved install dependencies of docker new, old and podman in debian and ubuntu

--- containers-aging/dependencies.sh
+++ containers-aging/dependencies.sh
@@ -9,10 +9,39 @@ readonly SYSTEM_VERSION="$VERSION_ID"
 
 KERNEL_VERSION=$(uname -r)
 
+if [ "$DISTRO_ID" == "ubuntu" ]; then
+  echo "por favor, em questao de uso do ubuntu caso não tenha movido a pasta do projeto para o dir /root/ faca isso e execute a partir do /root/!"
+
+  read -r -p "moveu a pasta software-aging para o diretorio /root/ [s]/[n]" correto
+  if [ "$correto" == "n" ]; then
+    echo "mova para /root/"
+    exit 1
+  fi
+fi
+
+IF_DISTRO_UBUNTU() {
+if [ "$DISTRO_ID" == "ubuntu" ]; then
+  echo "por padrao voce precisa logar como root para distro ubuntu server"
+  echo "isto é necessário para obter caminhos de variaveis definidas sempre a nível de user root"
+  echo "não se preocupe, será adicionado esta senha para root somente quando o script de dependencia estiver executando"
+  echo "ao finalizar a execucao o login para o user root será removido"
+
+  printf "\n%s\n" "Adicione uma nova senha para o user root abaixo: "
+  passwd root
+
+  echo "pronto! Faça login como root e execute novamente sem selecionar esta opcao" && exit 0
+else
+  echo "sua distro nao e ubuntu! Saindo....." && exit 1
+
+fi
+
+}
+
 ADD_UBUNTU_DOCKER_REPOSITORY() {
-  $(. /etc/os-release && echo "$VERSION_CODENAME") "stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-  apt-get update
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu" \
+  "$DISTRO_CODENAME stable" > /etc/apt/sources.list.d/docker.list
+
+  apt update
 }
 
 ADD_DEBIAN_DOCKER_REPOSITORY() {
@@ -21,22 +50,18 @@ ADD_DEBIAN_DOCKER_REPOSITORY() {
 }
 
 INSTALL_DOCKER_UBUNTU_OLD() {
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu" \
-    "$(ADD_UBUNTU_DOCKER_REPOSITORY)"
+  ADD_UBUNTU_DOCKER_REPOSITORY
 
   VERSION_STRING="5:20.10.13~3-0~ubuntu-$DISTRO_CODENAME"
-  apt-get install docker-ce="$VERSION_STRING" docker-ce-cli="$VERSION_STRING" containerd.io docker-buildx-plugin docker-compose-plugin
+  apt install docker-ce="$VERSION_STRING" docker-ce-cli="$VERSION_STRING" containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 }
 
 INSTALL_DOCKER_UBUNTU_NEW() {
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu" \
-    "$(ADD_UBUNTU_DOCKER_REPOSITORY)"
+  ADD_UBUNTU_DOCKER_REPOSITORY
 
   VERSION_STRING="5:26.0.1-1~ubuntu.$SYSTEM_VERSION~$DISTRO_CODENAME"
-  apt-get install docker-ce="$VERSION_STRING" docker-ce-cli="$VERSION_STRING" containerd.io docker-buildx-plugin docker-compose-plugin
+  apt install docker-ce="$VERSION_STRING" docker-ce-cli="$VERSION_STRING" containerd.io docker-buildx-plugin docker-compose-plugin -y
 }
 
 INSTALL_DOCKER_DEBIAN_OLD() {
@@ -46,7 +71,7 @@ INSTALL_DOCKER_DEBIAN_OLD() {
     ADD_DEBIAN_DOCKER_REPOSITORY
 
   VERSION_STRING="5:20.10.13~3-0~debian-bullseye"
-  apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+  apt install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin -y
 }
 
 INSTALL_DOCKER_DEBIAN_NEW() {
@@ -56,7 +81,7 @@ INSTALL_DOCKER_DEBIAN_NEW() {
     ADD_DEBIAN_DOCKER_REPOSITORY
 
   VERSION_STRING="5:26.0.1-1~debian.11~bullseye"
-  apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+  apt install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin -y
 }
 
 INSTALL_PYTHON_DEPENDENCIES() {
@@ -104,7 +129,24 @@ INSTALL_LIBRARIES_FOR_MONITORING() {
 
   read -r -p "choice: " choice
   if [ "$choice" -eq 1 ]; then
-    apt install linux-headers-"$KERNEL_VERSION" linux-image-"$KERNEL_VERSION"-dbg gnupg curl wget sysstat systemtap -y
+    apt install gnupg curl wget sysstat systemtap -y
+
+    if [ "$DISTRO_ID" == "ubuntu" ]; then
+      apt install ubuntu-dbgsym-keyring -y && {
+        echo "deb http://ddebs.ubuntu.com $DISTRO_CODENAME main restricted universe multiverse
+        deb http://ddebs.ubuntu.com $DISTRO_CODENAME-updates main restricted universe multiverse
+        deb http://ddebs.ubuntu.com $DISTRO_CODENAME-proposed main restricted universe multiverse" > "/etc/apt/sources.list.d/ddebs.list"
+
+        apt update
+      }
+
+      apt install linux-headers-"$KERNEL_VERSION" linux-image-"$KERNEL_VERSION"-dbgsym -y
+
+    else
+      apt install linux-headers-"$KERNEL_VERSION" linux-image-"$KERNEL_VERSION"-dbg -y
+
+    fi
+
     cp /proc/kallsyms /boot/System.map-"$KERNEL_VERSION"
 
   else
@@ -113,7 +155,7 @@ INSTALL_LIBRARIES_FOR_MONITORING() {
 }
 
 INSTALL_DOCKER_DEPENDENCIES() {
-  apt-get install ca-certificates curl
+  apt install ca-certificates curl -y
   install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   chmod a+r /etc/apt/keyrings/docker.asc
@@ -179,7 +221,7 @@ EOF
   fi
 
   apt install git gcc make curl wget pkg-config conmon crun containernetworking-plugins iptables -y
-  apt-get install -y libsystemd-dev libgpgme-dev libseccomp-dev -y
+  apt install -y libsystemd-dev libgpgme-dev libseccomp-dev -y
 
   wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
   rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
@@ -203,15 +245,15 @@ PODMAN_INSTALL() {
 }
 
 ADD_ROOT_PATH_PACKAGES() {
-  echo "export PKG_CONFIG_PATH=/usr/lib/pkgconfig" >>"$HOME"/.bashrc
-  echo "export PATH=\$PATH:/usr/local/go/bin" >>"$HOME"/.bashrc
-  echo "export PATH=\$PATH:/root/podman/bin" >>"$HOME"/.bashrc
+  echo "export PKG_CONFIG_PATH=/usr/lib/pkgconfig" >>"/root/".bashrc
+  echo "export PATH=\$PATH:/usr/local/go/bin" >>"/root/".bashrc
+  echo "export PATH=\$PATH:/root/podman/bin" >>"/root/".bashrc
 }
 
 ADD_HOME_PATH_PACKAGES() {
-  echo "export PKG_CONFIG_PATH=/usr/lib/pkgconfig" >>"$HOME"/.bashrc
-  echo "export PATH=\$PATH:/usr/local/go/bin" >>"$HOME"/.bashrc
-  echo "export PATH=\$PATH:/home/$(logname)/podman/bin" >>"$HOME"/.bashrc
+  echo "export PKG_CONFIG_PATH=/usr/lib/pkgconfig" >>"/home/$(logname)/".bashrc
+  echo "export PATH=\$PATH:/usr/local/go/bin" >>"/home/$(logname)/".bashrc
+  echo "export PATH=\$PATH:/home/$(logname)/podman/bin" >>"/home/$(logname)/".bashrc
 }
 
 MAIN() {
@@ -247,21 +289,31 @@ MAIN() {
 }
 
 reset
-printf "%s\n" "vai usar diretorio root, home ou já configurou as paths no .bashrc?"
-printf "%s\n" "Root - [1]"
-printf "%s\n" "Home - [2]"
+printf "%s\n" "vai usar ubuntu e/ou diretorio root, home ou já configurou as paths no .bashrc?"
+printf "%s\n" "Configurar Paths no dir root - [1]"
+printf "%s\n" "Configurar Paths no dir home - [2]"
 printf "%s\n" "Diretorio Configurado (prosseguir instalação) - [3]"
-printf "%s\n" "Sair - [4]"
+printf "%s\n" "Execute esta opcao ( primeiro ) caso esteja usando distro ubuntu - [4]"
+printf "%s\n" "Sair - [5]"
 
 read -r -p "choice: " diretorio
 if [ "$diretorio" -eq 1 ]; then
   ADD_ROOT_PATH_PACKAGES && echo "faca: ( source /root/.bashrc ) e execute novamente o mesmo codigo digitando a opção [3] ou [4]" && exit 0
 
 elif [ "$diretorio" -eq 2 ]; then
-  ADD_HOME_PATH_PACKAGES && echo "faca: ( source /root/.bashrc ) e execute novamente o mesmo codigo digitando a opção [3] ou [4]" && exit 0
+  ADD_HOME_PATH_PACKAGES && echo "faca: ( source /home/$(logname)/.bashrc ) e execute novamente o mesmo codigo digitando a opção [3] ou [4]" && exit 0
 
 elif [ "$diretorio" -eq 3 ]; then
+  if [ "$DISTRO_ID" == "ubuntu" ]; then
+    [[ "$(pwd)" != "/root" ]] && echo "execute a pasta a partir do diretorio /root" && exit 1
+  fi
   MAIN
+
+  printf "\n%s\n" "retirando acesso ao user root definido no inicio da execucao: " && sleep 3
+  passwd -dl root
+
+elif [ "$diretorio" -eq 4 ]; then
+  IF_DISTRO_UBUNTU
 
 else
   echo "saindo....." && exit 0
