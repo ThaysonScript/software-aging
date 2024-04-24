30f6bd5e3352c0148e718acceed36ce8ead7e665
Pedro Vinícius de Melo Silva <65138009+PedroViniciusMelo@users.noreply.github.com>
2024-04-24 08:27:16 -0300

    Update dependencies.sh

--- containers-aging/dependencies.sh
+++ containers-aging/dependencies.sh
@@ -135,11 +135,11 @@ INSTALL_LIBRARIES_FOR_MONITORING() {
 
       codename=$(lsb_release -c | awk  '{print $2}')
       tee /etc/apt/sources.list.d/ddebs.list << EOF
-      deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
-      deb http://ddebs.ubuntu.com/ ${codename}-updates  main restricted universe multiverse
-      deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
-      EOF
-      
+deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
+deb http://ddebs.ubuntu.com/ ${codename}-updates  main restricted universe multiverse
+deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
+EOF
+
       apt-get update
       apt install linux-headers-"$KERNEL_VERSION" linux-image-"$KERNEL_VERSION"-dbgsym -y
 
