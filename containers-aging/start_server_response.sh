3cfbdcf94a8c79333d5d5142be4c03db587bd811
ThaysonScript <thaysonguedes01@gmail.com>
2024-05-03 17:40:44 -0300

    organizations and fixing bugs

--- containers-aging/start_server_response.sh
+++ containers-aging/start_server_response.sh
@@ -2,25 +2,35 @@
 
 server_response="./software-aging-v2/rejuvenation/machine_resources_monitoring/server-response-time.sh"
 
-time_for_exit=$( sleep 31680 )
+pids=() # Array para armazenar os PIDs dos processos
 
-pids=() #Aqui deve ser um array
-
-trap "exit" SIGINT SIGTERM
+trap "EXIT" SIGINT SIGTERM
 
 EXIT() {
-	for pid in pids; do
-		kill "$pid"
-	done
+    echo "Exiting..."
+    for pid in "${pids[@]}"; do
+        kill "$pid"
+    done
+    exit 0
 }
 
-pids+=($($server_response "192.168.0.101:8080" "docker_novo_ubuntu_velho.csv" &))
-pids+=($($server_response "192.168.0.102:8080" "docker_novo_ubuntu_novo.csv" &))
-pids+=($($server_response "192.168.0.103:8080" "podman_novo_ubuntu_novo.csv" &))
-pids+=($($server_response "192.168.0.104:8080" "docker_novo_ubuntu_velho.csv" &))
+run_server_response() {
+    local ip_address="$1"
+    local output_file="$2"
+    "$server_response" "$ip_address" "$output_file" &
+    pids+=($!)  # Adiciona o PID do processo à lista
+}
+
+run_server_response "192.168.0.101:8080" "docker_novo_ubuntu_velho.csv"
+run_server_response "192.168.0.102:8080" "docker_novo_ubuntu_novo.csv"
+run_server_response "192.168.0.103:8080" "podman_novo_ubuntu_novo.csv"
+run_server_response "192.168.0.104:8080" "docker_novo_ubuntu_velho.csv"
 
 
-while true; do  # Ou um sleep de 11 dias
-    $time_for_exit
-    EXIT
-done
+# while true; do  # Ou um sleep de 11 dias
+#     $time_for_exit
+#     EXIT
+# done
+
+sleep 31680
+EXIT
