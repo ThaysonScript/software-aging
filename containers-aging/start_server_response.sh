578a4e0825494510bd99a3c591fcfa58f9fb49b8
ThaysonScript <thaysonguedes01@gmail.com>
2024-05-03 18:56:53 -0300

    fixing bugs

--- containers-aging/start_server_response.sh
+++ containers-aging/start_server_response.sh
@@ -21,12 +21,12 @@ run_server_response() {
     pids+=($!)  # Adiciona o PID do processo à lista
 }
 
-run_server_response "192.168.1.105:8080" "docker_antigo_ubuntu24.csv"
+run_server_response "192.168.1.105:8080/tools.descartes.teastore.webui/" "docker_antigo_ubuntu24.csv"
 
 # ADD MORE ADDRESSES AND OTHERS CSVS
 # run_server_response "192.168.1.105:8080" "docker_novo_ubuntu_novo.csv"
 # run_server_response "192.168.0.103:8080" "podman_novo_ubuntu_novo.csv"
 # run_server_response "192.168.0.104:8080" "docker_novo_ubuntu_velho.csv"
 
-sleep 31680
+sleep 31680     # equivale a 11 dias em segundos; favor verificar isso direito
 EXIT
