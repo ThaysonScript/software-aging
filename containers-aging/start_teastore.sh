b3a497fbf35e26a34c71fad4b73cf3dbc4ac561e
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2024-05-09 15:17:05 -0300

    Added sleep time for teastore

--- containers-aging/start_teastore.sh
+++ containers-aging/start_teastore.sh
@@ -6,22 +6,26 @@ software=$1
 
 $software run -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=10000" -p 10000:8080 -d --restart=always docker.io/descartesresearch/teastore-registry
 
+sleep 60
+
 $software run -p 3306:3306 -d --restart=always docker.io/descartesresearch/teastore-db
+
+sleep 60
 
 $software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=1111" -e "DB_HOST=$ip_address" -e "DB_PORT=3306" -p 1111:8080 -d --restart=always docker.io/descartesresearch/teastore-persistence
 
+sleep 60
+
 $software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=2222" -p 2222:8080 -d --restart=always docker.io/descartesresearch/teastore-auth
 
-$software run --name "teastore-recommender" -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=3333" -p 3333:8080 -d --restart=always docker.io/descartesresearch/teastore-recommender
+sleep 60
+
+$software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=3333" -p 3333:8080 -d --restart=always docker.io/descartesresearch/teastore-recommender
+
+sleep 60
 
 $software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=4444" -p 4444:8080 -d --restart=always docker.io/descartesresearch/teastore-image
 
+sleep 60
+
 $software run -e "REGISTRY_HOST=$ip_address" -e "REGISTRY_PORT=10000" -e "HOST_NAME=$ip_address" -e "SERVICE_PORT=8080" -p 8080:8080 -d --restart=always docker.io/descartesresearch/teastore-webui
-
-sleep 5
-
-$software stop teastore-recommender
-
-sleep 5
-
-$software start teastore-recommender
