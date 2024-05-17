89a3d006185f010c78daadb7799ed87660a66fc9
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2024-05-17 14:19:34 -0300

    Fixed ip address

--- containers-aging/start_server_response.sh
+++ containers-aging/start_server_response.sh
@@ -21,15 +21,15 @@ run_server_response() {
     pids+=($!)  # Adiciona o PID do processo à lista
 }
 
-run_server_response "192.168.1.105:8080" "debian12_docker_novo.csv"
-run_server_response "192.168.1.106:8080" "ubuntu22_docker_velho.csv"
-run_server_response "192.168.1.107:8080" "ubuntu24_podman.csv"
-run_server_response "192.168.1.108:8080" "debian12_docker_velho.csv"
-run_server_response "192.168.1.109:8080" "ubuntu22_podman.csv"
-run_server_response "192.168.1.110:8080" "ubuntu24_docker_novo.csv"
-run_server_response "192.168.1.111:8080" "debian12_podman.csv"
-run_server_response "192.168.1.112:8080" "ubuntu22_docker_novo.csv"
-run_server_response "192.168.1.113:8080" "ubuntu24_docker_velho.csv"
+run_server_response "192.168.0.105:8080" "debian12_docker_novo.csv"
+run_server_response "192.168.0.106:8080" "ubuntu22_docker_velho.csv"
+run_server_response "192.168.0.107:8080" "ubuntu24_podman.csv"
+run_server_response "192.168.0.108:8080" "debian12_docker_velho.csv"
+run_server_response "192.168.0.109:8080" "ubuntu22_podman.csv"
+run_server_response "192.168.0.110:8080" "ubuntu24_docker_novo.csv"
+run_server_response "192.168.0.111:8080" "debian12_podman.csv"
+run_server_response "192.168.0.112:8080" "ubuntu22_docker_novo.csv"
+run_server_response "192.168.0.113:8080" "ubuntu24_docker_velho.csv"
 
 sleep infinity
 EXIT
