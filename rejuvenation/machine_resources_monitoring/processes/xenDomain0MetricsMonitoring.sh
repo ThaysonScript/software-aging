c497b4f75fabec9c9d9ba59f897aca21fb659d51
ThaysonScript <thaysonguedes01@gmail.com>
2024-10-25 00:46:23 -0300

    fixing and reorganizations

--- rejuvenation/machine_resources_monitoring/processes/xenDomain0MetricsMonitoring.sh
+++ rejuvenation/machine_resources_monitoring/processes/xenDomain0MetricsMonitoring.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "NAME;STATE;CPU(sec);CPU(%);MEM(k);MEM(%);MAXMEM(k);MAXMEM(%);VCPUS;NETS;NETTX(k);NETRX(k);VBDS;VBD_OO;VBD_RD;VBD_WR;VBD_RSECT;VBD_WSECT;SSID" > "logs/xenDomain0MetricsMonitoring.csv"
 
 xentop -b -d 1 | while read -r line; do
     if echo "$line" | grep -q "Domain-0"; then
